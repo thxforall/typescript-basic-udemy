fdfd0e6b428161b548fd73a9054fcd163650c5f9
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-06-25 03:04:38 +0900

    basic typescript

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Your code goes here~');
+var add = function (num1, num2, showResult, phrase) {
+    // if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+    //   throw new Error('Incorrect input!!')
+    // }
+    var result = number1 + number2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+};
+var number1 = 5;
+var number2 = 10;
+var printResult = true;
+var resultPharse = 'Result is : ';
+var addResult = add(number1, number2, printResult, resultPharse);
+console.log(addResult);
